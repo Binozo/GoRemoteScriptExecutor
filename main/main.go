ce4fd8b16c20eba33fda52ff9a892efef922d79b
noahc laptop <noah.czaplewski05@gmail.com>
2022-08-06 11:36:08 +0200

    finished main.go

--- main/main.go
+++ main/main.go
@@ -1,65 +1,18 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"log"
-	"net/http"
-	"os"
-	"os/exec"
+	"RemoteScriptExecutor/pkg/arghandler"
+	"RemoteScriptExecutor/pkg/credentialsmanager"
+	"RemoteScriptExecutor/pkg/scriptmanager"
+	"RemoteScriptExecutor/pkg/server"
+	"RemoteScriptExecutor/pkg/taskmanager"
 )
 
 func main() {
-	password := getPassword()
-	http.HandleFunc("/run", func(w http.ResponseWriter, req *http.Request) {
-		scriptName := req.URL.Query().Get("script")
-
-		result := password == req.URL.Query().Get("password")
-
-		if !result {
-			fmt.Println("Password wrong")
-			w.Write([]byte("Password wrong"))
-			return
-		}
-
-		length := len(scriptName)
-		if length == 0 {
-			fmt.Println("No script name provided")
-			w.Write([]byte("No script name provided"))
-			return
-		}
-
-		cmd := exec.Command("/bin/bash", scriptName)
-		fmt.Println(cmd.Output())
-
-		w.Write([]byte("{\"result\":\"success\"}"))
-
-	})
-
-	http.ListenAndServe(":5123", nil)
-}
-
-func getPassword() string {
-	f, err := os.Open("creds.txt")
-
-	if err != nil {
-		log.Fatal(err)
-		fmt.Println(err)
-	}
-
-	defer f.Close()
-
-	scanner := bufio.NewScanner(f)
-
-	for scanner.Scan() {
-		return scanner.Text()
-	}
-
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-		fmt.Println(err)
-	}
-
-	return ""
-
+	arghandler.HandleArgs() // Note: HandleArgs is capable of terminating the entire application
+	taskmanager.Setup()
+	credentialsmanager.CheckIfPasswordIsSet() // Note: May terminates the application if the password hasn't been set correctly
+	scriptmanager.Setup()
+	server.RegisterRoutes()
+	server.Run()
 }
