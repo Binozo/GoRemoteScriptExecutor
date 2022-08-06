1a10f45d9bf748d2da19116437db6ceaf3c98764
noahc laptop <noah.czaplewski05@gmail.com>
2022-08-06 11:29:45 +0200

    added constants

---
+++ pkg/constants/constatns.go
@@ -0,0 +1,11 @@
+package constants
+
+const (
+	Port             = 5123
+	Name             = "GoRemoteScriptExecutor"
+	Version          = "0.1"
+	AliveArg         = "-testrun"
+	SetupPasswordArg = "-set-password"
+	AliveResponse    = "Hello World!"
+	EnvPassword      = "GoRemoteScriptExecutorP"
+)
