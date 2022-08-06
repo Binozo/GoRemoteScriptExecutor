6b9cc7832ff8094d4ae651ffb1ebc58cd8e7a78c
noahc laptop <noah.czaplewski05@gmail.com>
2022-08-06 11:29:13 +0200

    prepared system.go

---
+++ pkg/system/system.go
@@ -0,0 +1,8 @@
+package system
+
+import "log"
+
+// CheckForUpdate checks if an update for GoRemoteScriptExecutor is available on the official GitHub Repository and installs it
+func CheckForUpdate() {
+	log.Println("Checking for updates...")
+}
