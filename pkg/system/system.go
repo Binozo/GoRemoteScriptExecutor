6146e076455fcb2ea2202cc1af11818cb4200672
Binozo <70137898+Binozo@users.noreply.github.com>
2022-08-07 15:46:52 +0200

    system.go finalized CheckForUpdate

--- pkg/system/system.go
+++ pkg/system/system.go
@@ -1,8 +1,17 @@
 package system
 
 import (
+	"RemoteScriptExecutor/pkg/constants"
+	"context"
+	"github.com/google/go-github/v45/github"
+	"io"
 	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
 	"runtime"
+	"strings"
 )
 
 func GetArchitecture() string {
@@ -12,4 +21,16 @@ func GetArchitecture() string {
 // CheckForUpdate checks if an update for GoRemoteScriptExecutor is available on the official GitHub Repository and installs it
 func CheckForUpdate() {
 	log.Println("Checking for updates...")
+	client := github.NewClient(nil)
+	release, _, err := client.Repositories.GetLatestRelease(context.Background(), constants.Developer, constants.Name)
+	if err != nil {
+		log.Printf("Couldn't check for updates: %e. Trying again in one hour.", err)
+	}
+	newTagName := release.GetTagName()
+	currentTagName := "v" + constants.Version
+
+	if newTagName != currentTagName {
+		log.Printf("New version %s is available...", newTagName)
+		downloadUpdateAndInstall(release)
+	}
 }
