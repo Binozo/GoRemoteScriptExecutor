ce11a3a5f5de70e4579a7c92ca109870ecc9ca23
Binozo <70137898+Binozo@users.noreply.github.com>
2022-08-07 15:47:03 +0200

    system.go added downloadUpdateAndInstall

--- pkg/system/system.go
+++ pkg/system/system.go
@@ -34,3 +34,83 @@ func CheckForUpdate() {
 		downloadUpdateAndInstall(release)
 	}
 }
+
+func downloadUpdateAndInstall(release *github.RepositoryRelease) {
+	/* Example for asset list names:
+	[
+		goremotescriptexecutor_amd64
+		goremotescriptexecutor_arm
+		goremotescriptexecutor_arm64
+	]
+	*/
+	target := strings.ToLower(constants.Name) + "_" + GetArchitecture()
+	downloadUrl := ""
+	for _, asset := range release.Assets {
+		if asset.GetName() == target {
+			downloadUrl = asset.GetBrowserDownloadURL()
+			break
+		}
+	}
+	if downloadUrl == "" {
+		log.Printf("Couldn't find executable for architecture %s. Update aborted. Please compile it yourself or create an issue and demand a download for your architecture.", GetArchitecture())
+		return
+	}
+	log.Printf("Downloading update from %s", downloadUrl)
+	executableName := filepath.Base(os.Args[0])
+	fileName := executableName + ".new"
+	err := downloadFile(fileName, downloadUrl)
+	if err != nil {
+		log.Printf("Couldn't download update: %s. Retrying in one hour", err.Error())
+		return
+	}
+	log.Println("Update downloaded. Testing executable..")
+	err = os.Chmod(fileName, 0755)
+	fullPath, err := filepath.Abs(fileName)
+	if err != nil {
+		log.Printf("Couldn't get absolute path of executable: %s. Aborting update.", err.Error())
+		return
+	}
+	response, err := exec.Command(fullPath, constants.AliveArg).Output()
+	if err != nil {
+		log.Printf("Couldn't test executable: %s. Maybe it's corrupt or doesn't work correctly on your platform. Aborting update.", err.Error())
+		os.Remove(fileName)
+		return
+	}
+	if string(response) != constants.AliveResponse {
+		log.Printf("Executable doesn't work. Aborting update.")
+		os.Remove(fileName)
+		return
+	}
+	log.Println("Executable works. Installing update...")
+	err = os.Remove(executableName)
+	if err != nil {
+		log.Printf("Couldn't remove old executable: %s", err.Error())
+	}
+	err = os.Rename(fileName, executableName)
+	if err != nil {
+		log.Printf("Couldn't rename new executable: %s. Aborting update.", err.Error())
+		return
+	}
+	log.Println("Update installed. Restarting...")
+	os.Exit(2)
+}
+
+func downloadFile(filepath string, url string) error {
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Create the file
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	return err
+}
