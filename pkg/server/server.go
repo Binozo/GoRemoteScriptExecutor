dc13425b9f946efda93fee1e9ec9330687895986
Binozo <70137898+Binozo@users.noreply.github.com>
2022-08-07 11:33:17 +0200

    added runScriptHandler in server.go

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -18,8 +18,9 @@ var (
 
 func RegisterRoutes() {
 	r = mux.NewRouter()
-	r.HandleFunc("/", rootHandler)
-	r.HandleFunc("/scripts", scriptsHandler)
+	r.HandleFunc("/", rootHandler).Methods("GET")
+	r.HandleFunc("/scripts", scriptsHandler).Methods("GET")
+	r.HandleFunc("/runScript/{script}", runScriptHandler).Methods("GET")
 	http.Handle("/", r)
 }
 
@@ -75,4 +76,92 @@ func scriptsHandler(w http.ResponseWriter, r *http.Request) {
 		"scripts": scripts,
 	})
 	w.Write(json)
+}
+
+func runScriptHandler(w http.ResponseWriter, r *http.Request) {
+	if res := checkCredentials(w, r); res == false {
+		return
+	}
+	// Check if script name is in request
+	scriptName := mux.Vars(r)["script"]
+	blocking := r.URL.Query().Get("blocking") == "true"
+	responseOutput := r.URL.Query().Get("responseOutput") == "true"
+	if scriptName == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		json, _ := json.Marshal(map[string]string{
+			"status":  "error",
+			"message": "No script name provided",
+		})
+		w.Write(json)
+		return
+	}
+
+	// Check if the script exists
+	scripts, err := scriptmanager.GetScripts()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json, _ := json.Marshal(map[string]string{
+			"status":  "error",
+			"message": err.Error(),
+		})
+		w.Write(json)
+	}
+
+	scriptExists := false
+	for _, curScriptName := range scripts {
+		if curScriptName == scriptName {
+			scriptExists = true
+		}
+	}
+	if !scriptExists {
+		w.WriteHeader(http.StatusBadRequest)
+		json, _ := json.Marshal(map[string]string{
+			"status":  "error",
+			"message": "Script doesn't exist",
+		})
+		w.Write(json)
+		return
+	}
+
+	// Run the script
+	if !blocking {
+		go func() {
+			_, err := scriptmanager.ExecuteScript(scriptName)
+			if err != nil {
+				log.Println("Error while executing script:", err)
+			}
+		}()
+		w.WriteHeader(http.StatusOK)
+		json, _ := json.Marshal(map[string]string{
+			"status": "ok",
+			"mode":   "non-blocking",
+		})
+		w.Write(json)
+		return
+	}
+	output, err := scriptmanager.ExecuteScript(scriptName)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json, _ := json.Marshal(map[string]string{
+			"status":  "error",
+			"type":    "Error while executing script",
+			"message": err.Error(),
+		})
+		w.Write(json)
+		return
+	}
+	if responseOutput {
+		w.WriteHeader(http.StatusOK)
+		json, _ := json.Marshal(map[string]string{
+			"status": "ok",
+			"output": output,
+		})
+		w.Write(json)
+	} else {
+		w.WriteHeader(http.StatusOK)
+		json, _ := json.Marshal(map[string]string{
+			"status": "ok",
+		})
+		w.Write(json)
+	}
 }
